d5b3bd7d823922c2931805b2d210fc8db53b09f9
Ates Goral <ates.goral@shopify.com>
2019-07-20 17:38:17 -0500

    Make LEDs crisp

--- components/Led.js
+++ components/Led.js
@@ -18,9 +18,14 @@ class Led extends React.Component {
 
   componentDidMount() {
     const canvas = this.canvas.current;
+    const dpr = window.devicePixelRatio;
 
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientHeight;
+    canvas.width = canvas.clientWidth * dpr;
+    canvas.height = canvas.clientHeight * dpr;
+
+    const ctx = canvas.getContext('2d');
+
+    ctx.scale(canvas.width, canvas.height);
 
     const draw = (t) => {
       const MAX_SIGNAL_AGE = 1000;
@@ -34,7 +39,7 @@ class Led extends React.Component {
 
       const ctx = canvas.getContext('2d');
 
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.clearRect(0, 0, 1, 1);
 
       const signal = ports.read(this.props.port);
 
@@ -48,7 +53,9 @@ class Led extends React.Component {
       const opacity = 1 - Math.min(signalAge, MAX_SIGNAL_AGE) / MAX_SIGNAL_AGE;
 
       ctx.fillStyle = `rgba(${signal.color},${opacity})`;
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.beginPath();
+      ctx.arc(0.5, 0.5, 0.5, 0, Math.PI * 2);
+      ctx.fill();
     };
 
     requestAnimationFrame(draw);
